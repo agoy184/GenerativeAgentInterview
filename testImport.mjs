c486e9180fa8889f3dc4a21b8dbf7910dab38f43
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-08-24 23:20:53 -0700

    adding changes to tesimport

--- testImport.mjs
+++ testImport.mjs
@@ -1,26 +1,28 @@
+// How to use OpenAI with JS
+// https://rollbar.com/blog/chatgpt-api-with-javascript/
 // https://stackoverflow.com/questions/76917525/module-has-no-exported-member-when-importing-from-openai
 // https://github.com/openai/openai-node/discussions/217
 
 // New
 import OpenAI from 'openai';
 
-console.log("OpenAI:", OpenAI)
+// console.log("OpenAI:", OpenAI)
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 const apiKey = process.env.OPENAI_API_KEY
-console.log(apiKey)
+// console.log(apiKey)
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
 
-const question = "";
+const question = "Act like intern being interviewed. Tell me about your past job experiences as a game development programmer.  Tell me about the games you've made, and the tools that you use (Game Engines, Types of games).  Also tell me about your strengths and weaknesses";
 
 const chatCompletion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [{"role": "user", "content": question}],
 });
 console.log(chatCompletion.choices[0].message);
-console.log(chatCompletion.data.choices[0].message[content]);
+console.log(chatCompletion.choices[0].message[content]);
