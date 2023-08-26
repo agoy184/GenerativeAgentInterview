13eb7339c13961aac6db1583b77db5d61c4b9d76
bluemunchkin <mmannik@ucsc.edu>
2023-08-26 09:12:01 -0700

    Update MainLevel.js
    no work :(

--- src/scenes/MainLevel.js
+++ src/scenes/MainLevel.js
@@ -26,16 +26,82 @@ class MainLevel extends Phaser.Scene{
         });
     
         const data = await response.json();
-        console.log(data.choices[0].message.content)
+        //console.log(data.choices[0].message.content)
         return data.choices[0].message.content;
     }
 
     relevance(In,mem){
         // In is the player input string and mem is core memory
-        
-        //prompt GBT "on a scale of 1 to 100 rate the relevancy of these two statements  playerinput memory"
-        
+
+        // dicionary for sorting top memories 
+        let dic ={}
+
+
+        // if mem is a dictornary where key is the string and the value is the inportance
+        // depends on how memory is implemented 
+        // just need to iterate through mem to get strings and then importance value    
+        for( let key in mem){
+
+            //In should be player input and key is the memory 
+            let text= "on a scale of 1 to 10 rate the relevancy of these two statements only say a number. "+In+" "+key
+
+            console.log(text)
+
+            // put text through GBT
+            // if function is updated with memory then hopefully we should be able to leave it empty
+            let response = this.callChatGBT(text)
+
+            console.log(response)
+
+            // get numbers from string
+            let NumbersFromString=0
+            NumbersFromString= parseInt(response)
+
+            //console.log(NumbersFromString)
+
+            //find the relevance number 
+            let rel = NumbersFromString
+
+            
+            console.log(rel)
+
+
+
+            // get to total retrieval. simple way by just adding them.
+            let retrieval = rel + mem[key]
+
+            //if 10 elements in dict
+            if (Object.keys(dic).length == 10){
+                // if lowest value in dict is lower than current retrieval
+                if( Math.min(Object.values(dic)) < retrieval){
+
+                    //find the key
+                    let RepKey=Object.keys(obj).find(key => obj[key] === value)
+                    //remove that element
+                    dic.delete(RepKey)
+
+                    dic[key]=retrieval
+
+                }
+
+            }
+
+            else{
+                //add to new dic with new number
+                dic[key]=retrieval
+
+            }
+
+            break
+
+        }
+
+        //array of top 10 memories
+
         // return x amount of relevant mem
+
+        return Object.keys(dic)
+        
     }
 
 
@@ -43,8 +109,8 @@ class MainLevel extends Phaser.Scene{
         //CoreMemory= //PQ
 
         //this.callChatGBT("Give me recipes for cheese!")
-        //const response = this.callChatGBT("Give me recipes for cheese!");
-        //console.log(response)
+        const response = this.callChatGBT("Give me recipes for cheese!");
+        console.log(response)
 
         // get player input string 
         this.add.text(10, 10, 'Enter your question:', { font: '32px Courier', fill: '#ffffff' });
@@ -55,11 +121,11 @@ class MainLevel extends Phaser.Scene{
         {
 
             if (event.keyCode === 13){
-                playerInputtedString(textEntry.text)
+                this.playerInputtedString(textEntry.text)
 
                 // DEBUG no edits to CHAT GBT INPUT
-                const response = this.callChatGBT(textEntry.text)
-                console.log(response)
+                //const response = this.callChatGBT(textEntry.text)
+                //console.log(response)
                 textEntry.text = ""
             }
             else if (event.keyCode === 8 && textEntry.text.length > 0)
@@ -84,7 +150,32 @@ class MainLevel extends Phaser.Scene{
 
 
         //find relevant of memories probably top 10? (Michael) 
-        //RelMem=this.relevance(PlayerInput,CoreMemory) 
+
+
+        let PlayerInput="do you eat cats?"
+
+        let CoreMemory= {
+
+            "Steve likes dogs":   4,
+            "Steve worked at the cat cafe": 3,
+            "Steve has a freight licence":  6,
+            "Steve is a olympic gold medalist":  9,
+            "Steve burned a few houses down":  2,
+            "Steve really likes megazord":  9,
+            "Steve cant drive":  3,
+            "Steve thinks hes super cool":  8,
+            "Steve was a major part of watergate":  6,
+            "Steve voted for himslef in the last election":  8,
+            "Steve cant read":  5,
+            "Steve cant jump that good":  1,
+            "Steve eats cats":   5,
+        }
+
+
+        let RelMem=this.relevance(PlayerInput,CoreMemory) 
+
+        console.log(RelMem)
+
 
         // Pose question to AI in correct form
         
