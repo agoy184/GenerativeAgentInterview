f58d0fa887ed576dff79c60c1c0dadf3b3829a5e
Abel Goy <agoy@ucsc.edu>
2023-08-27 01:42:22 -0700

    Intern 1 Sprites + Load Code added

--- src/scenes/MainLevel.js
+++ src/scenes/MainLevel.js
@@ -140,6 +140,14 @@ class MainLevel extends Phaser.Scene{
     }
 
     create() {
+        // Add interviewee NPC sprite
+        // this.intern5 = this.add.sprite(355, 355, 'intern5').setScale(0.7);
+        // this.intern5 = this.add.sprite(355, 355, 'intern5').setScale(0.7);
+        // this.intern5 = this.add.sprite(355, 355, 'intern5').setScale(0.7);
+        // this.intern5 = this.add.sprite(355, 355, 'intern5').setScale(0.7);
+        this.intern5 = this.add.sprite(355, 355, 'intern5').setScale(0.7);
+
+
         //CoreMemory= //PQ
 
         this.CoreMemory= {
